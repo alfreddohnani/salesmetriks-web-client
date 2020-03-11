24b59a23db7edd3b65be76b8d9adf00e7514ebde
Alfred Doh-Nani <icommasbabyface@gmail.com>
2020-03-11 20:10:42 +0000

    -ch: Undo changes to vuetify options

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,20 @@ export default {
   vuetify: {
     customVariables: ["~/assets/variables.scss"],
     treeShake: true,
-    optionsPath: "./vuetify.options.js"
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
   },
   /*
    ** Build configuration
