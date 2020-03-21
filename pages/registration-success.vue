1ad8915d9fb82c9c9081552861612a4981482d1d
Alfred Doh-Nani <icommasbabyface@gmail.com>
2020-03-21 08:15:18 +0000

    -ch: Add registration success page

---
+++ pages/registration-success.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-container>
+      <div class="background">
+        <v-card class="mx-auto" color="#367100" dark max-width="400">
+          <v-card-text class="headline font-weight-bold">
+            Your account registration was successful! An email has been sent to
+            you with your activation link. To proceed, click the activation link
+            to activate your account. Then
+            <v-btn :to="{ name: 'login' }">Login</v-btn>
+          </v-card-text>
+        </v-card>
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RegistrationSuccesPage",
+  layout: "landing-pages"
+};
+</script>
+
+<style scoped>
+.background {
+  background-image: url("/undraw_welcome_cats_thqn.svg");
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+}
+</style>
